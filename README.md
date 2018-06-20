f0f43d1fc1e9bb2b2a13bce6be4cb4203ab03849
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2018-06-20 23:34:32 +0800

    Initial import (work in progress).

---
+++ README.md
@@ -0,0 +1,5 @@
+# LXQt Archiver
+
+## Overview
+
+A Qt file archiver based on engrampa.
---
+++ src/fr-resources.h
@@ -0,0 +1,7 @@
+#ifndef __RESOURCE_fr_H__
+#define __RESOURCE_fr_H__
+
+#include <gio/gio.h>
+
+extern GResource *fr_get_resource (void);
+#endif
