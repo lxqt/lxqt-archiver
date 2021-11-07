6509b117bc605828d749b7742f019a687f94e0f4
Weblate <noreply@weblate.org>
2021-11-07 07:08:23 +0100

    Weblate commits
    Co-authored-by: NightFeather <ss900328@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lxqt-archiver/zh_TW/
    Translation: LXQt Desktop/LXQt-archiver

--- src/translations/lxqt-archiver_zh_TW.ts
+++ src/translations/lxqt-archiver_zh_TW.ts
@@ -263,7 +263,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
     <message>
         <location filename="../mainwindow.ui" line="163"/>
         <source>Main Toolbar</source>
-        <translation>主要工具列</translation>
+        <translation>主選單列</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="191"/>
